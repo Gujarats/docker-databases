b7cfa7a999ec9f4902059507c8e2490dd3c801c7
gujarats <gujarat.santana@gmail.com>
2017-11-08 11:18:49 +0700

    update install.sh to create shortcut cli

--- README.md
+++ README.md
@@ -7,14 +7,3 @@ After that you can run this command on this directory :
 docker-compose up -d
 
 ```
-
-If you are using linux you can specify shorcut in your `.bashrc` file like this : 
-
-```shell
-#databases
-alias mongo-docker='docker exec -it mongo-docker mongo'
-alias mysql-docker='docker exec -it mysql-docker mysql -uroot -proot'
-alias redis-docker='docker exec -it redis-docker redis-cli'
-```
-
-Put those lines in the botton of your `.bashrc` file. usually this file ins located in `~/.bashrc`.
--- install.sh
+++ install.sh
@@ -9,3 +9,11 @@ sudo mkdir /usr/local/redis
 sudo chown -R gujarat:gujarat /usr/local/mysql
 sudo chown -R gujarat:gujarat /usr/local/mongodb
 sudo chown -R gujarat:gujarat /usr/local/redis
+
+# create shortcut cli
+echo "" >> ~/.bashrc
+echo "#databases" >> ~/.bashrc
+echo "alias mongo-docker='docker exec -it mongo-docker mongo'" >> ~/.bashrc
+echo "alias mysql-docker='docker exec -it mysql-docker mysql -uroot -proot'" >> ~/.bashrc
+echo "alias redis-docker='docker exec -it redis-docker redis-cli'" >> ~/.bashrc
+
